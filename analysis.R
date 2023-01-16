d5ddbeeddf15dc632a6b35476b258274839a1b58
C-Heininger <c.heininger@medunigraz.at>
2023-01-16 22:44:39 +0100

    playing around with tss

--- analysis.R
+++ analysis.R
@@ -1,9 +1,10 @@
-# Libraries
+# Libraries ####
 
 library(tidyverse)
 library(GenomicRanges)
+library(biomaRt)
 
-# Define functions
+# Define functions ####
 
 subset_quants <- function(df) {
     df <- df[,c(1:3,7:9)]
@@ -37,8 +38,12 @@ enhancer_quant_id <- function(df) {
     return(df)
 }
 
+sort_df <- function(df) {
+    df[order(df$chr),]
+}
 
-# first read all ROSE output files into a list
+
+# first read all ROSE output files into a list and make GRanges ####
 
 rose_out_list <- list.files(path = "./data", pattern = "*_AllStitched.table.txt", full.names = TRUE) %>% 
     setNames(., str_extract(., "([:digit:]+h\\_[:upper:]+)")) %>% 
@@ -51,12 +56,17 @@ rose_out_list <- lapply(rose_out_list, filter_chr_col)
 
 rose_out_list <- lapply(rose_out_list, rose_enhancer_id)
 
-
-# rose_gr_list <- GRangesList(lapply(rose_out_list, makeGRangesFromDataFrame, ignore.strand = FALSE, keep.extra.columns = TRUE))
+rose_out_list <- lapply(rose_out_list, sort_df)
 
 rose_gr_list <- lapply(rose_out_list, makeGRangesFromDataFrame, ignore.strand = FALSE, keep.extra.columns = TRUE)
 
-# read enhancer quantification data into a list
+
+
+
+
+
+
+# read enhancer quantification data into a list and make GRanges ####
 
 enhancer_quant_list <- list.files(path = "./data", pattern = "enhancer_quant_*", full.names = TRUE) %>% 
     setNames(., str_extract(., "([:digit:]+h\\_[:upper:]+)")) %>% 
@@ -70,7 +80,7 @@ enhancer_quant_gr_list <- lapply(enhancer_quant_list, makeGRangesFromDataFrame, 
 
 
 
-# pseudo-code
+# Merge all ranges per timepoint by overlap ####
 
 merged_0h <- as.data.frame(mergeByOverlaps(enhancer_quant_gr_list[[1]], rose_gr_list[[1]]))
 
@@ -92,10 +102,71 @@ merged_list <- list(
 merged_list <- lapply(merged_list, enhancer_quant_id)
 
 
+# Read gene expression TPM data ####
+
+tpm_matrix <- read_delim("./data/tpm_matrix_gene.txt", delim = "\t", col_names = TRUE)
+
+ensembl <- useEnsembl(biomart = "genes",
+                      dataset = "mmusculus_gene_ensembl",
+                      mirror = "www")
+
+gene_tss <- getBM(attributes = c("external_gene_name", 
+                                 "ensembl_gene_id",
+                                 "chromosome_name",
+                                 "start_position",
+                                 "end_position",
+                                 "transcription_start_site",
+                                 "strand",
+                                 "gene_biotype"),
+                  filters = "external_gene_name",
+                  values = tpm_matrix$gene,
+                  mart = ensembl)
 
 
+is_unique <- function(x) length(unique(x)) == 1
+
+# All transcripts of a gene should have the same start_position, i.e. the 5´
+# most recorded position for any of the transcripts
+stopifnot(tapply(gene_tss$start_position, gene_tss$ensembl_gene_id, is_unique))
+
+# All transcripts of a gene should have the same end_position, i.e. the 3´
+# most recorded position for any of the transcripts
+stopifnot(tapply(gene_tss$end_position, gene_tss$ensembl_gene_id, is_unique))
+
+# Therefore start_position should always be less than end_position, no matter
+# the strand of the gene
+stopifnot(mapply(function(x, y) x < y,
+                 gene_tss$start_position,
+                 gene_tss$end_position))
+
+# Reduce to most upstream TSS per gene -----------------------------------------
+
+# Detach biomaRt before loading dplyr because they both define `select`
+detach("package:biomaRt")
+suppressPackageStartupMessages(library("dplyr"))
+
+tss_per_gene <- gene_tss %>%
+    group_by(ensembl_gene_id, external_gene_name, chromosome_name,
+             strand, gene_biotype) %>%
+    summarize(tss = if (all(strand == 1)) min(start_position) else max(end_position)) %>%
+    ungroup() %>%
+    arrange(ensembl_gene_id)
+
+# Format -----------------------------------------------------------------------
+
+tss_out <- tss_per_gene %>%
+    mutate(strand = ifelse(strand == 1, "+", "-")) %>%
+    select(gene_id = ensembl_gene_id,
+           gene_name = external_gene_name,
+           chr = chromosome_name,
+           tss,
+           strand,
+           biotype = gene_biotype)
 
 
+gr_0h <- rose_gr_list$`0h_AL`
+
+gr_0h$genes <- 
 
 
 
